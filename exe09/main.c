e7887e5c727febb2fbad94f61828d6dfeb27388b
Lucas-Lipani <lucasphenrique@hotmail.com>
2021-08-24 11:08:10 -0300

    Falta só o 09

--- exe09/main.c
+++ exe09/main.c
@@ -1,8 +1,50 @@
 #include <stdio.h>
+#include <string.h>
+
+
+typedef struct diario{
+   char nome[100];
+   float nota;
+}diario;
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int n;
+   printf("Quantos?\n");
+   scanf(" %d", &n);
+   diario aluno[n];
+
+   // loop responsável de preencher os alunos
+   for (int i = 0; i < n; i++){
+      fgets(aluno[i].nome,100,stdin);
+      scanf(" %f", &aluno[i].nota);
+   }
+
+   // loop responsável de calcular a média dos alunos cadastrados
+   float media = 0;
+
+   for (int i = 0; i < n; i++)
+      media += aluno[i].nota;
+   
+
+   media = media/n;
+
+   // loop responsável de selecionar as 3 maiores notas
+/*
+   for (int i = 0; i < n; i++){
+      if (aluno[i].nota > aluno[0].nota){
+         aluno[2].nome = aluno[1].nome;
+         aluno[2].nota = aluno[1].nota;
+         aluno[1].nome = aluno[0].nome;
+         aluno[1].nota = aluno[0].nota;
+         aluno[0].nome = aluno[i].nome;
+         aluno[0].nota = aluno[i].nota;
+      }
+   }
+   */
+   for (int i = 0; i < n; i++){
+      printf("Nome: %s --- Nota: %f\n", aluno[i].nome, aluno[i].nota);
+   }
+   //printf("NOTA MEDIA = %.1f\n%s %.1f\n%s %.1f\n%s %.1f\n", media, aluno[0].nome, aluno[0].nota, aluno[1].nome, aluno[1].nota, aluno[2].nome, aluno[2].nota);
+
    return 0;
 }
--- exe10/main.c
+++ exe10/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
-   int n1, n2;
-   scanf("%i %i", &n1, &n2);
-   printf("SOMA = %i\n", n1+n2);
+   int v[10];
+
+   for (int i = 0; i < 10; i++){
+      scanf(" %d", &v[i]);
+   }
+
+   int v_invertido[10];
+
+   for (int i = 0, j = 9; i < 10; i++,  j--){
+      v_invertido[j] = v[i];
+   }
+
+   for(int i = 0; i < 10; i++){
+      printf("%d\n", v_invertido[i]);
+   }
    return 0;
 }
